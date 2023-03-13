eda630945b513fb1a2e2210852a70471a0dd9c39
tiannaj23 <tiannaj23@gmail.com>
2023-03-13 16:34:15 -0400

    added store.js file

---
+++ Develop/db/store.js
@@ -0,0 +1,7 @@
+const util = require('util');
+const fs = require('fs');
+
+const uuidv1 = require('uuid/v1');
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
